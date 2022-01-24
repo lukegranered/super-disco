f57e0927643c632a8967f159ba9bf11d1328cbc1
Luke G <lukejgranered@gmail.com>
2022-01-24 16:49:53 -0600

    event listener on description box

---
+++ Develop/script.js
@@ -0,0 +1,38 @@
+var date = moment().format("MMMM dddd DD YYYY");
+
+
+$("#currentDay").text(date);
+console.log($("#currentDay").innerText);
+
+$(".container").on("click", ".description", function(){
+    var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+        .addClass("textarea")
+        .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".container").on("blur", "textarea", function(){
+    var text = $(this)
+        .val()
+        .trim();
+
+    // var status = $(this)
+    //     .closest(".container")
+    //     .attr("id")
+
+    // var index = $(this)
+    //     .closest(".container-item")
+    //     .index();
+
+    var descriptionBlock = $("<p>")
+        .addClass("col-10 description")
+        .text(text);
+
+    $(this).replaceWith(descriptionBlock);
+});
